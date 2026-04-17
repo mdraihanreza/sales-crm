ec97fa9dfdcc3578d1ca5bbf2e11a22fbed82eac
Elvira Infotech Developer <developer@elvirainfotech.com>
2026-04-17 12:15:25 +0530

    Make Daphne port configurable for Render

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-exec daphne -b 0.0.0.0 -p 8000 config.asgi:application
+exec daphne -b 0.0.0.0 -p ${PORT:-8000} config.asgi:application
